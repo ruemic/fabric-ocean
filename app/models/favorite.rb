779e96aa56bba15456afdf3e0722f14a2dc15e1b
ruemic <rumicdesign@gmail.com>
2012-10-09 03:31:34 -0700

    fix favorite model scope

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -6,6 +6,5 @@ class Favorite < ActiveRecord::Base
   belongs_to :user
   # attr_accessible :title, :body
   attr_accessible :user_id, :product_id
-  validates_uniqueness_of :product_id
-
+  validates_uniqueness_of :product_id, :scope => :user_id
 end
